0fa9f8eba1a2b7e2dea119bcadf97161aebc0147
Quentin <quentingosset7500@hotmail.com>
2023-01-03 22:30:59 +0100

    Initialization

--- script/cron.ts
+++ script/cron.ts
@@ -1,7 +1,7 @@
 import * as cron from "node-cron";
 const { exec } = require("child_process");
 console.log(`CRON STARTED`);
-const task1 = cron.schedule("0 0 0 * * *", () => {
+const task1 = cron.schedule("0 0 */4 * * *", () => {
   exec(`npm run full`, (error: any, stdout: any, stderr: any) => {
     if (error) {
       console.error(`FAILED WITH ERROR : ${error}`);
